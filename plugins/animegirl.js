3727bac866c73d47d68f2fd02038e020490ebec8
Koyeb-MD <ddilisha456@gmail.com>
2024-12-06 21:06:08 +0530

    C  R  A  S  H  -  M  D

---
+++ plugins/animegirl.js
@@ -0,0 +1,71 @@
+const axios = require('axios');
+const { cmd, commands } = require('../command');
+
+cmd({
+    pattern: "animegirl2",
+    desc: "Fetch a random anime girl image.",
+    category: "fun",
+    react: "👧",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const apiUrl = `https://api.waifu.pics/sfw/waifu`;
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *CRASH-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©CRASH-MD BY ♤DILISHA GIMSHAN♤*' }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`*Error Fetching Anime Girl image*: ${e.message}`);
+    }
+});
+const axios = require('axios');
+const { cmd, commands } = require('../command');
+
+cmd({
+    pattern: "animegirl1",
+    desc: "Fetch a random anime girl image.",
+    category: "fun",
+    react: "👧",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const apiUrl = `https://api.waifu.pics/sfw/waifu`;
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *CRASH-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SILENT-SOBX-MD BY SILENTLOVER432*' }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`*Error Fetching Anime Girl image*: ${e.message}`);
+    }
+});
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "anime",
+    desc: "anime the bot",
+    category: "main",
+    react: "⛱️",
+    filename: __filename
+},
+
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let dec = `❮❮❮ *CRASH DEVELOPER DILISHA ANIME PHOTOS* ❯❯❯`
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/b26f27aa5daaada031b90.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/51b44e4b086667361061b.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/7d165d73f914985542537.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/3d9732d2657d2d72dc102.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/8daf7e432a646f3ebe7eb.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/7514b18ea89da924e7496.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/ce9cb5acd2cec7693d76b.jpg`},caption:dec},{quoted:mek});
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
