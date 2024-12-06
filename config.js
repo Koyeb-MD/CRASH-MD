ddc1440370d2a81a5335753f7907235394f57042
Koyeb-MD <ddilisha456@gmail.com>
2024-12-06 21:10:25 +0530

    C  R  A  S  H  -  M  D

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
-MONGODB: process.env.MONGODB || "enter mongodb url",
+SESSION_ID: process.env.SESSION_ID || "2JlwmATD#zLegNqDof9o5a9R08pD-_eaz35j3zTtGQaV_BNEuhOY",
+MONGODB: process.env.MONGODB || "mongodb://mongo:MgRxoQTXWnKFtprOrocHAeucSEKWBtoE@junction.proxy.rlwy.net:45585",
 };
